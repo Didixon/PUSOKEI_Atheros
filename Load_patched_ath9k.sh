3997f6189fce9f754bb814f210eab617e60e11e9
Didixon <yk77mdb852@privaterelay.appleid.com>
2026-08-08 23:25:55 +0100

    Update Load_patched_ath9k.sh

--- Load_patched_ath9k.sh
+++ Load_patched_ath9k.sh
@@ -19,11 +19,17 @@ sudo insmod ath9k/ath9k_hw.ko
 sudo insmod ath9k/ath9k_common.ko
 sudo insmod ath9k/ath9k_htc.ko
 
+# ---- CHECK loaded modules ----
+
 lsmod | grep ath
 
 dmesg | tail -30
 
+# ---- CHECK WiFi adapter ----
+
 iw reg get
+
+iwlist wlan1 frequency
 
 # ---- To go back to stock later, unload the patched set the same way, ----
 # ---- then reload with modprobe instead of insmod: ----
